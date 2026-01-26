bc760120a073da7f8fe0973bec81826f7dfeaa1a
YalinSun <yalinsun2022@gmail.com>
2026-01-26 05:09:01 -0800

    update main.tf

--- assignment2/main.tf
+++ assignment2/main.tf
@@ -40,6 +40,13 @@ resource "aws_security_group" "ssh" {
     protocol    = "tcp"
     cidr_blocks = [var.ssh_cidr]
   }
+  ingress {
+    description = "Gin app on 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
@@ -61,3 +68,7 @@ data "aws_ami" "al2023" {
 output "ec2_public_dns" {
   value = aws_instance.demo-instance.public_dns
 }
+output "ec2_public_ip" {
+  value = aws_instance.demo-instance.public_ip
+}
+
